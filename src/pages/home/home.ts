79013df418acc804da3f93a825f3f2e5b0eec62c
Yordy Lopez <yordy.lopez@brickcontrol.com>
2019-02-05 18:55:58 -0400

    primer commit, creacion de template ionic

---
+++ src/pages/home/home.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
